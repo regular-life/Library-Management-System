0ec31757643a6c1d5e61cd561268eefb9eafec15
Yash Bhardwaj <133621845+regular-life@users.noreply.github.com>
2023-09-06 00:42:04 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,1 +1,17 @@
+package library_management;
 
+public class Book
+{
+    String title ;
+    String author ;
+    int book_id ;
+    int status ;
+
+    Book(String title, String author, int book_id)
+    {
+        this.title = title ;
+        this.author = author ;
+        this.book_id = book_id ;
+        this.status = 0 ;
+    }
+}
